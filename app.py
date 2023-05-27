3e4288e7c5a711c36da361b968482739a9d11f56
Anahit Khachatryan <anahitkhachatryan@Anahits-MacBook-Air.local>
2023-05-27 16:55:20 +0400

    minor change

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 st.set_page_config(page_title='Shein', page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 my_model = functions.read_model()
-st.write("success")
+st.success("Successfull!")
 shein_df, cos_similarities_df = functions.read_data()
 
 st.write("# Recommender System - Shein")
